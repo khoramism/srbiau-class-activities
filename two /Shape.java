a24be0303cf76b1808887052d96e287bbc800a54
khoramism <khoramism@gmail.com>
2022-02-26 22:13:47 +0330

    The first Tamrin is done

--- two /Shape.java
+++ two /Shape.java
@@ -2,13 +2,25 @@
 import java.util.Arrays;
 import java.lang.Math;
 public class Shape {
-    public static  String circle(int r) {
-        double mohit , masahat; 
-        mohit = 2 * r * Math.PI; 
-        masahat = r * r * Math.PI;
-        double[] answers = {mohit, masahat};
-        return Arrays.toString(answers);
+    double mohit , masahat; 
+    public static String circle(int r) {
+        Shape cir = new Shape(); 
+        cir.mohit = 2 * r * Math.PI; 
+        cir.masahat = r * r * Math.PI;
+        String cirs = String.format("masahat in dayere %f va mohit aan %f ast!", cir.mohit, cir.masahat); // String value  
+        return cirs; 
     }
+    public static String rectangle(int length , int width) {
+        Shape rect = new Shape(); 
+        rect.mohit = 2 * (length + width);
+        rect.masahat = length * width;
+        String recs = String.format("masahat in Mostatil ya Morabae %f va mohit aan %f ast!", rect.mohit, rect.masahat); // String value  
+        return recs;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(circle(3));    }    
+        System.out.println(circle(3));    
+        System.out.println(rectangle(3, 10));    
+    }    
 }
