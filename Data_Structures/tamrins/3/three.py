bfe806a866db8526283047b88f92c04ba43dd598
khoramsim-personal-laptop <khoramism@khoramism.com>
2023-01-10 12:02:48 +0330

    third session tamrins

---
+++ Data_Structures/tamrins/3/three.py
@@ -0,0 +1,17 @@
+def PostfixToInfix(expression): 
+    stack = [] 
+  
+    for char in expression: 
+        if char.isalpha(): 
+            stack.append(char) 
+  
+        else: 
+            val1 = stack.pop() 
+            val2 = stack.pop() 
+            stack.append("(" + val2 + char + val1 + ")") 
+  
+    return stack.pop() 
+  
+if __name__ == "__main__": 
+    expression = "ab+ef*g*-"
+    print(PostfixToInfix(expression))
